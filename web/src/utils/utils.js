2cff156624e7dd371adf237d304918fd05f609b9
Evan Ovadia <verdagon@gmail.com>
2017-04-21 08:55:07 -0700

    derp

--- web/src/utils/utils.js
+++ web/src/utils/utils.js
@@ -207,8 +207,7 @@ Utils.getParameterByName = function(name, defaultValue) {
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
   var results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return defaultValue;
+  if (!results || !results[2]) return defaultValue;
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
