f2b265cea290b26ad68fa3b8f3feff88a158f099
Isaac Good <github@isaacgood.com>
2017-05-05 15:48:00 -0700

    Move backend_test, add notification settings. Add unit test scaffold

---
+++ backend/main_methods_test.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import mock
+import unittest
+
+import main
+
+class TestMethods(unittest.TestCase):
+  
+  def test_send_chat_message(self):
+    self.assertEqual(1,1)
+
+if __name__ == '__main__':
+  unittest.main()
+
+# vim:ts=2:sw=2:expandtab
