5245878e2824f4469742ca39466c88558fdf982b
Isaac Good <github@isaacgood.com>
2017-05-06 16:57:56 -0700

    Change player volunteer to a list of roles

--- backend/constants.py
+++ backend/constants.py
@@ -1,5 +1,10 @@
 # Fill out with value from
 # https://firebase.corp.google.com/project/trogdors-29fa4/settings/database
-FIREBASE_SECRET = "ZiD9uLhDnrLq2n416MjWjn0JOrci6H0oGm7bKyVN"
+FIREBASE_SECRET = ""
 FIREBASE_EMAIL = ""
 ALLEGIANCES = ('horde', 'resistance', 'none')
+TEST_ENDPOINT = 'http://localhost:8080'
+PLAYER_VOLUNTEER_ARGS = (
+    'helpAdvertising', 'helpLogistics', 'helpCommunications', 'helpModerator',
+    'helpCleric', 'helpSorcerer', 'helpAdmin', 'helpPhotographer', 'helpChronicler',
+    'helpServer', 'helpClient', 'helpMobile')
