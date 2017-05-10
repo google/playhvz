a164197e61a00e0e2577d95476ac1b3c4de901dd
Isaac Good <github@isaacgood.com>
2017-05-10 01:07:12 -0700

    Rename secrets.py to make gitignore work

--- backend/secrets_.py
+++ backend/secrets_.py
@@ -1,4 +1,5 @@
 # Fill out with value from
 # https://console.firebase.google.com/project/trogdors-29fa4/settings/serviceaccounts/databasesecrets
+# Then rename from secrets_.py to secrets.py
 FIREBASE_SECRET = ''
 FIREBASE_EMAIL = ''
