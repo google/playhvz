1755e45ab2f273ffda88191ac92b08ec447adfb0
Isaac Good <IsaacG@users.noreply.github.com>
2017-05-09 22:22:40 -0700

    Drop (now leaked) credentials

--- backend/constants.py
+++ backend/constants.py
@@ -1,7 +1,7 @@
 # Fill out with value from
 # https://console.firebase.google.com/project/trogdors-29fa4/settings/serviceaccounts/databasesecrets
-FIREBASE_SECRET = "d0h365xNGuJU7A3rB65bAJ4v59VMLJiaNY5R0D6C"
-FIREBASE_EMAIL = "harshmodi@google.com"
+FIREBASE_SECRET = ""
+FIREBASE_EMAIL = ""
 ALLEGIANCES = ('horde', 'resistance', 'none')
 TEST_ENDPOINT = 'http://localhost:8080'
 PLAYER_VOLUNTEER_ARGS = (
