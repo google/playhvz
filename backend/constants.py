3984bebee258ad52be9974fefeba951fac5e1089
Isaac Good <github@isaacgood.com>
2017-05-16 11:45:21 -0700

    Fix test data, tweak port and setup_secrets

--- backend/constants.py
+++ backend/constants.py
@@ -1,5 +1,5 @@
 ALLEGIANCES = ('horde', 'resistance', 'none')
-TEST_ENDPOINT = 'http://localhost:8080'
+TEST_ENDPOINT = 'http://localhost:8008'
 PLAYER_VOLUNTEER_ARGS = (
     'helpAdvertising', 'helpLogistics', 'helpCommunications', 'helpModerator',
     'helpCleric', 'helpSorcerer', 'helpAdmin', 'helpPhotographer', 'helpChronicler',
