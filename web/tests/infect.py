096bcac382dab8c2be33499c030fbfdb40bea795
Evan Ovadia <verdagon@gmail.com>
2017-05-28 12:00:18 -0700

    added infect webdriver

--- web/tests/infect.py
+++ web/tests/infect.py
@@ -25,30 +25,6 @@ try:
 
   driver.FindElement([[By.NAME, 'ChatRoom: Horde ZedLink']])
 
-  # driver.Click([[By.ID, 'createGame']])
-
-  # driver.SendKeys(
-  #     [[By.ID, 'idInput'], [By.TAG_NAME, 'input']],
-  #     'mygame')
-
-  # driver.SendKeys(
-  #     [[By.ID, 'nameInput'], [By.TAG_NAME, 'input']],
-  #     'My Game')
-
-  # driver.SendKeys(
-  #     [[By.ID, 'stunTimerInput'], [By.TAG_NAME, 'input']],
-  #     '60')
-  
-  # driver.Click([[By.ID, 'gameForm'], [By.ID, 'done']])
-
-  # driver.ExpectContains(
-  #     [[By.TAG_NAME, 'ghvz-game-details'], [By.ID, 'name']],
-  #     'My Game')
-
-  # driver.ExpectContains(
-  #     [[By.TAG_NAME, 'ghvz-game-details'], [By.ID, 'stunTimer']],
-  #     '60')
-
 finally:
   # driver.Quit()
   pass
