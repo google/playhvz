0c03f1829eac553388b600f214374a876f8acdeb
AliengirlLiv <oliviagracewatkins@gmail.com>
2017-06-08 22:31:33 -0700

    Added modify game webdriver test

--- web/tests/modifygame.py
+++ web/tests/modifygame.py
@@ -14,91 +14,66 @@ try:
   driver.ExpectContains([[By.NAME, 'playerPagesJoined']], "Rules")
   driver.ExpectContains([[By.NAME, 'playerPagesJoined']], "FAQ")
 
-  # Go to the Admin dashboard
+  # # Go to the Admin dashboard
   driver.Click([[By.NAME, 'Admin Dashboard']])
 
   # Open up rules, type something.
   driver.FindElement([[By.NAME, 'Rules']])
   driver.Click([[By.TAG_NAME, 'ghvz-desktop-admin-page'], [By.NAME, 'rules-icon'], [By.ID, 'icon']]) 
   driver.SendKeys(
-      [[By.NAME, 'HELLO WORLD!!!'], [By.TAG_NAME, 'textarea']],
+      [[By.NAME, 'admin-page'], [By.TAG_NAME, 'textarea']],
       'rules are cools')
 
   # If you click Cancel, the new words shouldn't show up.
-  driver.Click([[By.NAME, 'HELLO WORLD!!!'],[By.ID, 'cancel']])
+  driver.Click([[By.NAME, 'admin-page'],[By.ID, 'cancel']])
   driver.DontExpectContains(
       [[By.ID, 'rulesForm']],
       'rules are cools')
 
-  # # Open up rules, type something different.
+  # Open up rules, type something different.
   driver.FindElement([[By.NAME, 'Rules']])
   driver.Click([[By.TAG_NAME, 'ghvz-desktop-admin-page'], [By.NAME, 'rules-icon'], [By.ID, 'icon']]) 
   driver.SendKeys(
-      [[By.NAME, 'HELLO WORLD!!!'], [By.TAG_NAME, 'textarea']],
+      [[By.NAME, 'admin-page'], [By.TAG_NAME, 'textarea']],
       'rules are cools when you save them')
 
-  # # If you click Save, the new words should show up.
-  driver.Click([[By.NAME, 'HELLO WORLD!!!'],[By.ID, 'cancel']])
-  driver.DontExpectContains(
-      [[By.ID, 'rulesForm']],
+  # If you click Save, the new words should show up.
+  driver.Click([[By.NAME, 'admin-page'],[By.ID, 'done']])
+  driver.ExpectContains(
+      [[By.ID, 'rules']],
       'rules are cools when you save them')
 
-  # Open up Game settings, set a new stun timer
+  # Open up Game settings, change each field
   driver.Click([[By.NAME, 'game-icon'], [By.ID, 'icon']])
-  driver.SendKeys(
-      [[By.ID, 'gameForm'], [By.TAG_NAME, 'input']],
-      'grobble forgbobbly')
+  driver.SendKeys([[By.ID, 'form-section-game-name'], [By.ID, 'input']], 'A Quick and Certain Death to Humanity')
+  driver.SendKeys([[By.ID, 'form-section-game-stunTimer'], [By.TAG_NAME, 'input']], 100)
 
-  # # If you click Cancel, the new stun timer shouldn't show up.
-  # driver.Click([[By.ID, 'infect']])
-  # driver.ExpectContains(
-  #     [[By.NAME, 'victimName']],
-  #     'Jack Slayer the Bean Slasher')
+  # If you click Cancel, the new changes shouldn't show up.
+  driver.Click([[By.ID, 'gameForm'],[By.ID, 'cancel']])
+  driver.DontExpectContains([[By.NAME, 'game-name']], 'A Quick and Certain Death to Humanity')
+  driver.DontExpectContains([[By.NAME, 'game-stunTimer']], "100")
 
-  # # Open up settings, set a new stun timer.
-  # driver.Click([[By.NAME, 'drawerDashboard']])  
-  # driver.SendKeys(
-  #     [[By.ID, 'lifeCodeInput'], [By.TAG_NAME, 'input']],
-  #     'grobble forgbobbly')
+  # # Open up Game settings, change all the fields again.
+  driver.Click([[By.NAME, 'game-icon'], [By.ID, 'icon']])
+  driver.SendKeys([[By.ID, 'form-section-game-name'], [By.ID, 'input']], 'Welcome to the Zombies-Have-Hunger Games')
+  driver.SendKeys([[By.ID, 'form-section-game-stunTimer'], [By.TAG_NAME, 'input']], 42)
 
   # # If you click Save, the new stun timer should show up.
-  # driver.Click([[By.ID, 'infect']])
-  # driver.ExpectContains(
-  #     [[By.NAME, 'victimName']],
-  #     'Jack Slayer the Bean Slasher')
+  driver.Click([[By.ID, 'gameForm'],[By.ID, 'done']])
+  driver.ExpectContains([[By.NAME, 'game-name']], 'Welcome to the Zombies-Have-Hunger Games')
+  driver.ExpectContains([[By.NAME, 'game-stunTimer']], "42")
 
-  # # Go to the FAQ page
-  # driver.Click([[By.NAME, 'drawerProfile']])
+  # Go to the FAQ page
+  driver.Click([[By.NAME, 'FAQ']])
 
-  #   # Open up settings, set a new stun timer
-  # driver.Click([[By.NAME, 'drawerDashboard']])  
-  # driver.SendKeys(
-  #     [[By.ID, 'lifeCodeInput'], [By.TAG_NAME, 'input']],
-  #     'grobble forgbobbly')
+  # TODO: Add in FAQ change when it has been implemented.
 
-  # # If you click Cancel, the new stun timer shouldn't show up.
-  # driver.Click([[By.ID, 'infect']])
-  # driver.ExpectContains(
-  #     [[By.NAME, 'victimName']],
-  #     'Jack Slayer the Bean Slasher')
-
-  # # Open up settings, set a new stun timer.
-  # driver.Click([[By.NAME, 'drawerDashboard']])  
-  # driver.SendKeys(
-  #     [[By.ID, 'lifeCodeInput'], [By.TAG_NAME, 'input']],
-  #     'grobble forgbobbly')
-
-  # # If you click Save, the new stun timer should show up.
-  # driver.Click([[By.ID, 'infect']])
-  # driver.ExpectContains(
-  #     [[By.NAME, 'victimName']],
-  #     'Jack Slayer the Bean Slasher')
-
-  # # Sign in as every user type, make sure all can see rules on dashboard + have rules page
-  # players = ['zella', 'deckerd', 'moldavi', 'drake', 'zeke', 'jack']
-  # for player in players:
-  #   driver.SwitchUser("jack")
-  #   driver.FindElement([[By.TAG_NAME, 'ghvz-infect']])
+  # Sign in as every user type, make sure all can see rules on dashboard + have rules page
+  players = ['deckerd', 'moldavi', 'drake', 'zeke', 'jack','zella']
+  for player in players:
+    driver.SwitchUser("jack")
+    driver.ExpectContains([[By.ID, 'rulesForm']], 'rules are cools when you save them')
+    driver.Click([[By.NAME, 'Rules']])
 
 finally:
   # driver.Quit()
