dbda465a7da50ee6a16c55c008cc0ec79a44ee6f
Evan Ovadia <verdagon@gmail.com>
2017-06-10 17:36:05 -0700

    derp

---
+++ backend/config_.py
@@ -0,0 +1,9 @@
+# Copy this file secrets_.py to a new file called secrets.py
+# Then follow the instructions in README to fill in these values.
+FIREBASE_SECRET = ''
+FIREBASE_EMAIL = ''
+IONIC_TOKEN = ''
+IONIC_APPID = ''
+IONIC_SECURITY_TAG = ''
+FIREBASE_CONFIG = { ... }
+FAKE_USER_IDS = { ... }
