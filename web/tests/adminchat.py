8c62f071d8acc9329778e9adc6c6884984b5f661
SquirrelThief <kshook@utk.edu>
2017-06-11 18:17:32 -0700

    Add test for creating chat with admins

--- web/tests/adminchat.py
+++ web/tests/adminchat.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 
 driver = setup.MakeDriver()
 driver.WaitForGameLoaded()
-driver.SwitchPage('chat')
 
 try:
   playerNames = {
@@ -15,23 +14,36 @@ try:
         'jack': 'Jack Slayer the Bean Slasher'
       }
 
-  adminNames = {
-        'zella': 'Zella the Ultimate',
-        'moldavi': 'Moldavi the Moldavish',
-    }
-
-  def testAdminChat(playersInChat, playersNotInChat, chatName):
+  def testCreateAdminChat(adminPlayers, nonAdminPlayers):
     try: 
 
-      print "hello there"
+      actingPlayer = nonAdminPlayers[0]
+      driver.SwitchUser(actingPlayer)
+
+      driver.Click([[By.NAME, 'drawerChat']]) # Change to chat page
+
+      driver.FindElement([[By.NAME, 'admin-chat']]) # Check that the admin chat button exists
+      driver.Click([[By.NAME, 'admin-chat']])
+      chatName = playerNames[actingPlayer] + ' & HvZ CDC'
+      driver.FindElement([[By.NAME, 'ChatRoom: %s' % chatName]]) # Check that the chat exists
+
+      # TODO: dom-if hides the element, so it's there but hidden which is probably why
+      # DontFindElement finds it
+      #driver.DontFindElement([[By.NAME, 'admin-chat']]) # Check that the admin chat button is gone
+
+      for admin in adminPlayers:
+        driver.SwitchUser(admin)
+        driver.FindElement([[By.NAME, 'drawer-' + chatName]])
+
+      #raise AssertionError("Test still in development, this is expected")
       
     finally:
       pass
 
-  # HORDE CHAT ROOM - only declared zombies should view
-  zombiePlayers = ['drake', 'zeke']
-  nonZombiePlayers = ['reggie', 'minny', 'zella', 'deckerd', 'moldavi', 'jack']
-  testAdminChat(zombiePlayers, nonZombiePlayers, 'Horde ZedLink')
+  # Run admin test
+  adminPlayers = ['zella', 'moldavi']
+  nonAdminPlayers = ['deckerd', 'drake', 'zeke', 'jack']
+  testCreateAdminChat(adminPlayers, nonAdminPlayers)
 
   driver.Quit()
 
