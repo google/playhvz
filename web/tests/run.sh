be1be408892041392fc69a2a84fdae15774c587d
Verdagon <verdagon@gmail.com>
2017-06-17 16:49:24 -0700

    Groupselector (#123)
    Adds the new group-field which uses a dialog instead of that BS third party POC paper-dropdown-input

--- web/tests/run.sh
+++ web/tests/run.sh
@@ -59,8 +59,8 @@ desktop_and_mobile_tests() {
 fake_and_remote_tests() {
 	client_url=$1
 	password=$2
-	# use_remote=0
-	# desktop_and_mobile_tests $client_url $password $use_remote
+	use_remote=0
+	desktop_and_mobile_tests $client_url $password $use_remote
 	use_remote=1
 	desktop_and_mobile_tests $client_url $password $use_remote
 }
