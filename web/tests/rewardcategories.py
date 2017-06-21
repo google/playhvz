1569ab9a184eb2a24cdf46de0669c7efc7e44b90
AliengirlLiv <oliviagracewatkins@gmail.com>
2017-06-20 19:49:00 -0700

    mostly done w. rewards

--- web/tests/rewardcategories.py
+++ web/tests/rewardcategories.py
@@ -13,43 +13,48 @@ try:
   driver.Click([[By.NAME, 'admin-rewards-card'], [By.ID, 'add']])
 
   # Make reward category.
-  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'nameInput'], [By.TAG_NAME, 'input']], "Lord Gnasher the Hungry Reward for Ravenous Zombies")
+  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'nameInput'], [By.TAG_NAME, 'input']], "Lord Gnasher the Hungry Reward")
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'shortNameInput'], [By.TAG_NAME, 'input']], "Gnasher")
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'pointsInput'], [By.TAG_NAME, 'input']], "2")
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'limitPerPlayerInput'], [By.TAG_NAME, 'input']], "2")
-  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'badgeImageUrlInput'], [By.TAG_NAME, 'input']], "http://www.google.com")
-  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'descriptionInput'], [By.TAG_NAME, 'textarea']], "A way to show off to your friends how much you love eating humans.")
+  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'badgeImageUrlInput'], [By.TAG_NAME, 'input']], "google.com")
+  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'descriptionInput'], [By.TAG_NAME, 'textarea']], "Show off how much you love eating humans!")
   driver.Click([[By.NAME, 'admin-rewards-card'], [By.ID, 'done']])
 
   # Verify that it shows up
-  driver.ExpectContains([[By.NAME, 'row-Gnasher']], 'Lord Gnasher the Hungry Reward for Ravenous Zombies')
+  driver.ExpectContains([[By.NAME, 'row-Gnasher']], 'Lord Gnasher the Hungry Reward')
   driver.ExpectContains([[By.NAME, 'row-Gnasher']], 'Gnasher')
   driver.ExpectContains([[By.NAME, 'row-Gnasher']], '2')
-  driver.ExpectContains([[By.NAME, 'row-Gnasher']], 'http://www.google.com')
+  driver.ExpectContains([[By.NAME, 'row-Gnasher']], 'google.com')
 
   # Edit the category
   driver.Click([[By.NAME, 'row-Gnasher'], [By.ID, 'menu']])
   driver.Click([[By.NAME, 'row-Gnasher'], [By.NAME, 'menu-item-Edit']])
+  driver.Backspace([[By.NAME, 'admin-rewards-card'], [By.ID, 'nameInput'], [By.TAG_NAME, 'input']], 30)
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'nameInput'], [By.TAG_NAME, 'input']], "Good Flosser Badge")
+  driver.Backspace([[By.NAME, 'admin-rewards-card'], [By.ID, 'shortNameInput'], [By.TAG_NAME, 'input']], 7)
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'shortNameInput'], [By.TAG_NAME, 'input']], "Flosser")
+  driver.Backspace([[By.NAME, 'admin-rewards-card'], [By.ID, 'pointsInput'], [By.TAG_NAME, 'input']], 1)
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'pointsInput'], [By.TAG_NAME, 'input']], "3")
+  driver.Backspace([[By.NAME, 'admin-rewards-card'], [By.ID, 'limitPerPlayerInput'], [By.TAG_NAME, 'input']], 1)
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'limitPerPlayerInput'], [By.TAG_NAME, 'input']], "900")
+  driver.Backspace([[By.NAME, 'admin-rewards-card'], [By.ID, 'badgeImageUrlInput'], [By.TAG_NAME, 'input']], 10)
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'badgeImageUrlInput'], [By.TAG_NAME, 'input']], "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Zombie-156055.svg/2000px-Zombie-156055.svg.png")
+  driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'descriptionInput'], [By.TAG_NAME, 'textarea']], 41)
   driver.SendKeys([[By.NAME, 'admin-rewards-card'], [By.ID, 'descriptionInput'], [By.TAG_NAME, 'textarea']], "A reward you get for brushing and flossing after eating humans.")
   driver.Click([[By.NAME, 'admin-rewards-card'], [By.ID, 'done']])
 
-  # Generate 200 rewards.
-  driver.Click([[By.NAME, 'row-Gnasher'], [By.ID, 'menu']])
-  driver.Click([[By.ID, 'row-Gnasher'], [By.NAME, 'menu-item-Show Reward']])
+  # Generate 22 rewards.
+  driver.Click([[By.NAME, 'row-Flosser'], [By.ID, 'menu']])
+  driver.Click([[By.ID, 'row-Flosser'], [By.NAME, 'menu-item-Show Rewards']])
   driver.SendKeys([[By.ID, 'newReward']], "22")
 
-  # Check that the rewards show up
+  # Check that at least one shows up #TODO(aliengirl) - is there a better way to do this?
+  driver.ExpectContains([[By.NAME, 'rewardsTable']], '(unclaimed)')
 
-  
-
-  # # Export 200 rewards.
-  # driver.Click([[By.NAME, 'row-Gnasher'], [By.ID, 'menu']])
-  # driver.Click([[By.ID, 'row-Gnasher'], [By.NAME, 'menu-item-Export Reward']])
+  # Export the rewards.
+  driver.Click([[By.NAME, 'row-Flosser'], [By.ID, 'menu']])
+  driver.Click([[By.ID, 'row-Flosser'], [By.NAME, 'menu-item-Export Reward']])
 
 finally:
   pass
