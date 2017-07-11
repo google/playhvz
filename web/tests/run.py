957e9bc68652fdb720b3c49d671e3f7a0fa97cca
AliengirlLiv <aliengirl@google.com>
2017-07-11 03:55:59 -0700

    Add stun timer webdriver and insert stun timer into everyone's profiles

--- web/tests/run.py
+++ web/tests/run.py
@@ -42,6 +42,7 @@ def generateTest(url, password, files, useRemote, useMobile):
 		args += " -m"
 
 	testFiles = [
+		"stuntimer", # out of alphabetical order b/c this is long, so we want to run it in parallel with others
 		"adminchat",
 		"adminguns",
 		"adminplayers",
@@ -64,7 +65,8 @@ def generateTest(url, password, files, useRemote, useMobile):
 		"requests",
 		"rewardcategories",
 		"selfinfect",
-		"startgame"
+		"startgame",
+		"signout"
 	]
 
 	if len(files) > 0:
