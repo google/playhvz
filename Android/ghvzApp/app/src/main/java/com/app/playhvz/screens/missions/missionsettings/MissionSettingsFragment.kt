01fdcb62201044645c218253f280807f90ede21c
SquirrelThief <kimfshook@gmail.com>
2020-03-24 00:37:01 -0700

    Add initial mission settings fragment
    Add initial layout and fragment for mission settings and creating a mission

---
+++ Android/ghvzApp/app/src/main/java/com/app/playhvz/screens/missions/missionsettings/MissionSettingsFragment.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.app.playhvz.screens.missions.missionsettings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import androidx.emoji.widget.EmojiEditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.app.playhvz.R
+import com.app.playhvz.common.globals.SharedPreferencesConstants
+import com.app.playhvz.firebase.classmodels.Mission
+import com.google.android.material.button.MaterialButton
+
+/** Fragment for showing a list of missions.*/
+class MissionSettingsFragment : Fragment() {
+    companion object {
+        private val TAG = MissionSettingsFragment::class.qualifiedName
+    }
+
+    lateinit var nameText: EmojiEditText
+    lateinit var submitButton: MaterialButton
+
+    val args: MissionSettingsFragmentArgs by navArgs()
+    var gameId: String? = null
+    var missionId: String? = null
+    var missionDraft: Mission? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        missionId = args.missionId
+        val sharedPrefs = activity?.getSharedPreferences(
+            SharedPreferencesConstants.PREFS_FILENAME,
+            0
+        )!!
+        gameId = sharedPrefs.getString(SharedPreferencesConstants.CURRENT_GAME_ID, null)
+        setupObservers()
+        setupToolbar()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_mission_settings, container, false)
+        submitButton = view.findViewById(R.id.submit_button)
+        submitButton.setOnClickListener {
+            submitMission()
+        }
+        nameText = view.findViewById(R.id.mission_name)
+        nameText.doOnTextChanged { text, start, count, after ->
+            when {
+                text.isNullOrEmpty() || text.isBlank() -> {
+                    submitButton.isEnabled = false
+                }
+                else -> {
+                    submitButton.isEnabled = true
+                }
+            }
+        }
+        return view
+    }
+
+    fun setupToolbar() {
+        val toolbar = (activity as AppCompatActivity).supportActionBar
+        if (toolbar != null) {
+            if (missionId == null) {
+                toolbar.title = context!!.getString(R.string.mission_settings_create_mission_title)
+                return
+            }
+            toolbar.title = context!!.getString(R.string.mission_settings_title)
+            toolbar.setDisplayHomeAsUpEnabled(false)
+        }
+    }
+
+    private fun setupObservers() {
+        if (gameId == null) {
+            return
+        }
+    }
+
+    private fun submitMission() {
+        val name = nameText.text
+    }
+}
